0c9702c8cc28c66996fe071aeb9a73f568c66715
juba <juba.jeremiah@gmail.com>
2025-09-25 22:50:12 +0100

    New update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Dashboard } from "./pages/dashboard"
 import { Contacts } from "./pages/contacts"
+import { Wallet } from "./pages/wallet"
 import { HashRouter as Router, Routes, Route } from "react-router-dom"
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Route path='/' element={ <Dashboard /> }></Route>
       <Route path='/contacts' element={ <Contacts /> }></Route>
       <Route path='/configuration' element={ <Contacts /> }></Route>
+      <Route path='/wallets' element={ <Wallet /> }></Route>
     </Routes>
   </Router>
   
--- src/pages/contacts.jsx
+++ src/pages/contacts.jsx
@@ -1,20 +1,48 @@
+/* import all the icons in Free Solid, Free Regular, and Brands styles */
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { library } from '@fortawesome/fontawesome-svg-core'
-import "../styles/maincontainer.css"
-/* import all the icons in Free Solid, Free Regular, and Brands styles */
 import { fas } from '@fortawesome/free-solid-svg-icons'
 import { far } from '@fortawesome/free-regular-svg-icons'
 import { fab } from '@fortawesome/free-brands-svg-icons'
 library.add(fas, far, fab)
-import { Link } from 'react-router-dom'
 import { Leftsidebar } from '../components/leftsidebar'
-
+import { Inputbox } from '../components/inputbox'
 import "../styles/contacts.css"
+import "../styles/maincontainer.css"
+
 
 export function Contacts(){
     return(
         <div className='mainContainer'>
-            <Leftsidebar />
+            <Leftsidebar  dashboard="dashboard2" contacts="contacts2" configuration="configuration2" wallet="wallet2"/>
+            <div className='contactbody'>
+
+                <div className='smheader'>
+                    <h2>Contacts</h2>
+                </div>
+
+                {/* Search Bar */}
+                <div className='searchcontainer'>
+                    <div className='search2'>
+                        <FontAwesomeIcon icon="fa-solid fa-magnifying-glass" className='searchFA'/>
+                        <input type="search" placeholder='Search' />
+                    </div>
+                    <div className='addcontainer'>
+                        <FontAwesomeIcon icon="fa-solid fa-plus" />
+                        <span>Add</span>
+                    </div>
+                </div>
+
+                <div>
+                    
+                </div>
+
+                {Array.from({ length: 4 }, () => (
+                        <Inputbox />
+                ))}
+
+            </div>
+
         </div>
 
     )
