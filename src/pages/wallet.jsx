86954733c508d04bf7f3e08bf8154b6ba783f8fa
haybdev <officewebb01@gmail.com>
2025-09-29 16:06:39 +0100

    wallet Funding page completed

--- src/pages/wallet.jsx
+++ src/pages/wallet.jsx
@@ -1,24 +1,35 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
 
 /* import all the icons in Free Solid, Free Regular, and Brands styles */
-import { fas } from '@fortawesome/free-solid-svg-icons'
-import { far } from '@fortawesome/free-regular-svg-icons'
-import { fab } from '@fortawesome/free-brands-svg-icons'
-library.add(fas, far, fab)
-import "../styles/wallet.css"
-import { Link } from 'react-router-dom'
-import { Leftsidebar } from '../components/leftsidebar'
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { far } from "@fortawesome/free-regular-svg-icons";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+library.add(fas, far, fab);
+import "../styles/wallet.css";
+import { Link } from "react-router-dom";
+import { Leftsidebar } from "../components/leftsidebar";
 
-
-export function Wallet(){
-    return(
-   
-        <div className='mainContainer'>
-            {/* Left side bar */}
-            <Leftsidebar  dashboard="dashboard3" contacts="contacts3" configuration="configuration3" wallet="wallet3"/>
-
-  
+export function Wallet() {
+  return (
+    <div className="mainContainer">
+      {/* Left side bar */}
+      <Leftsidebar
+        dashboard="dashboard3"
+        contacts="contacts3"
+        configuration="configuration3"
+        wallet="wallet3"
+      />
+      <div className="wallet-funding">
+        <h2>Wallet Funding</h2>
+        <div className="wallet-input">
+          <label htmlFor="amount">
+            Enter Amount
+            <input type="number" name="amount" id="amount" placeholder="$"/>
+          </label>
+          <button>Fund Wallet</button>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
