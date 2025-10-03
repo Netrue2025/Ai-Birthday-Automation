f7b546733c84ce2e816eed1b9c490f8bf15967dc
juba <juba.jeremiah@gmail.com>
2025-10-03 16:30:32 +0100

    update with logo

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,13 +1,18 @@
 import "../styles/home.css"
 import { Button } from "../components/button"
 import { Link } from "react-router-dom"
+import logo from "../assets/logo.PNG"
 
 
 export function Home(){
     return(
         <section>
             <header>
+                <figure>
+                    <img src={logo} alt="logo" />
+                </figure>
                 <nav>
+
                     <a href="https://ai-birthday-automation.vercel.app" target="_blank">
                         <Button className="signup_btn" name="Sign Up"/>
                     </a>
