7b84e45d0eca780e7109e4ab7c705a10f59946a2
juba <juba.jeremiah@gmail.com>
2025-10-05 14:42:03 +0100

    Update with animation

--- src/App.jsx
+++ src/App.jsx
@@ -6,22 +6,34 @@ import { HashRouter as Router, Routes, Route } from "react-router-dom"
 import { Home } from "./pages/home"
 import { Signup } from "./pages/sign-up"
 import { Signin }from"./pages/signin"
-
+import { Canvas } from "@react-three/fiber"
+import { Stars, OrbitControls } from "@react-three/drei"
+import "./App.css"
 
 function App() {
   return ( 
-  <Router>
-    <Routes>
-       <Route path='/' element={ <Home /> }></Route>
-      <Route path='/dashboard' element={ <Dashboard /> }></Route>
-      <Route path='/contacts' element={ <Contacts /> }></Route>
-      <Route path='/configuration' element={ <Configuration /> }></Route>
-      <Route path='/wallet' element={ <Wallet /> }></Route>
-      <Route path='/signup' element={ <Signup /> }></Route>
-      <Route path='/signin' element={ <Signin /> }></Route>
+    <>
+      <div className="relative">
+        <Canvas>
+           <ambientLight intensity={0.5} />
+          <Stars radiu={50} count={2500} factor={4} fade speed={2} />
+          <OrbitControls />
+        </Canvas>
+      </div>
+      <Router>
+        <Routes>
+          <Route path='/' element={ <Home /> }></Route>
+          <Route path='/dashboard' element={ <Dashboard /> }></Route>
+          <Route path='/contacts' element={ <Contacts /> }></Route>
+          <Route path='/configuration' element={ <Configuration /> }></Route>
+          <Route path='/wallet' element={ <Wallet /> }></Route>
+          <Route path='/signup' element={ <Signup /> }></Route>
+          <Route path='/signin' element={ <Signin /> }></Route>
 
-    </Routes>
-  </Router>
+        </Routes>
+      </Router>
+    </>
+
   
   )
 }
--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,13 +1,11 @@
 
 
-
 export function Button(props){
     return(
         <div>
             
             <button onClick={props.click} className={props.className}>{props.name}</button>
-        
-            
+    
         </div>
     )
 }
