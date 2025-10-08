ad43c33674a85f7afb4adbe1f02ef1d2da560784
haybdev <officewebb01@gmail.com>
2025-10-08 01:07:57 +0100

    initial commit: signin page

--- src/pages/signin.jsx
+++ src/pages/signin.jsx
@@ -1,7 +1,36 @@
+import { Link } from "react-router-dom";
+import "../styles/signin.css";
+
 export function Signin() {
-    return(
-        <div>
-            <h1>This is the Log-in page</h1>
+  return (
+    <form className="signin">
+      <div className="signin_container">
+        <div className="signin_title">
+          <h1>ENVIO AUTO MESSAGES</h1>
+          <h2>Welcome back</h2>
+          <p>Sign in to continue</p>
         </div>
-    )
+        <div className="signin_input">
+          <input type="email" placeholder="Email Address" />
+          <input type="password" placeholder="Password" />
+        </div>
+        <div className="remember_me">
+          <div className="remember_me-chkbx">
+            <input type="checkbox" id="chkbx" />
+            <label htmlFor="chkbx">Remember me</label>
+          </div>
+          <Link to={"#"} className="forgot_password-link">
+            <span>Forgot password?</span>
+          </Link>
+        </div>
+        <button className="signin-btn">Sign in</button>
+        <div className="signin_footer">
+          <p>New here?</p>
+          <Link to={"/signup"} className="signup-link">
+            <span>Create an account</span>
+          </Link>
+        </div>
+      </div>
+    </form>
+  );
 }
